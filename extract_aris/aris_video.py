ad04a7dcfb2258190546f907cfddc773bf40074d
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2020-12-31 18:12:03 -0800

    Test aris to video extraction

--- extract_aris/aris_video.py
+++ extract_aris/aris_video.py
@@ -5,5 +5,6 @@ from pyARIS import pyARIS
 def main():
   filename = "./2020-05-09_103000.aris"
   aris_data, frame = pyARIS.DataImport(filename)
+  pyARIS.VideoExport(aris_data, 'test.mp4', start_frame=2650, end_frame=2720, vbr=13)
 
 main()
