b637d9819f460da8ac779ee53d724bbe4a13c220
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-01-26 19:04:46 -0800

    Update label conversion

--- utils/jq-labels.sh
+++ utils/jq-labels.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-jq '[.[] | .attributes[].mutable = false]' $1 | \
-    jq '[.[] | .attributes[].values = (.attributes[].values | split("\n"))]'
+jq '[.[] | .attributes[].mutable |= false]' $1 | \
+    jq '[.[] | .attributes[].values |= split("\n")]'
