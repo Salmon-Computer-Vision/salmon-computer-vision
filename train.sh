72adda68b9cb80fea016bfdf5fe9e5ed11797ec4
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-02-05 01:04:46 -0800

    Add optional arguments to train.sh

--- train.sh
+++ train.sh
@@ -1,8 +1,33 @@
 #!/usr/bin/env bash
 
-PIPELINE_CONFIG_PATH=models/my_ssd_mobilenet_v1_fpn/pipeline.config
-MODEL_DIR=models/my_ssd_mobilenet_v1_fpn
+usage() { echo "Usage: $0 [-c path/to/pipeline.config] [-m path/to/model/dir]" 1>&2; exit 1; }
+
+pipeline=models/my_faster_rcnn_resnet50_v1_640x640/pipeline.config
+model_dir=models/my_faster_rcnn_resnet50_v1_640x640
+
+while getopts ":c:m:h" o; do
+    case "${o}" in
+        c)
+            pipeline=${OPTARG}
+            ;;
+        m)
+            model_dir=${OPTARG}
+            ;;
+        h)
+            usage
+            ;;
+        \?)
+            echo "Invalid option"
+            usage
+            ;;
+        :)
+            echo "Missing argument"
+            usage
+            ;;
+    esac
+done
+
 python ~/models/research/object_detection/model_main_tf2.py \
-    --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
-    --model_dir=${MODEL_DIR} \
+    --pipeline_config_path=${pipeline} \
+    --model_dir=${model_dir} \
     --alsologtostderr
