2b6e1826551810dfa823b2f2f965d6bd75cb0ccf
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-02-05 00:18:11 -0800

    Fix run docker script and add prelim pipeline config

--- run_tf_docker.sh
+++ run_tf_docker.sh
@@ -8,7 +8,7 @@ dataset=$1
 
 [ "$#" -eq 1 ] || die "1 argument required for the dataset path, $# provided"
 
-docker run --gpus all -it -v $PWD:code \
-    -v "${dataset}:data" \
-    -w code \
+docker run --gpus all -it -v "$PWD:/home/tensorflow/code" \
+    -v "${dataset}:/home/tensorflow/data" \
+    -w /home/tensorflow/code \
     od
