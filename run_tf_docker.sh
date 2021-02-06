fc52c58360e16b7378645dd4f41cc0bfcd324031
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-02-05 20:25:58 -0800

    Update config for 640x640 ver and add options to docker

--- run_tf_docker.sh
+++ run_tf_docker.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
-die () {
-    echo >&2 "$@"
-    exit 1
-}
+usage() { echo "Usage: $0 [-d docker-image:tag] path/to/dataset" 1>&2; exit 1; }
+
+while getopts ":d:h" o; do
+    case "${o}" in
+        d)
+            image=${OPTARG}
+            ;;
+        h)
+            echo "Help"
+            usage
+            ;;
+        \?)
+            echo "Invalid option"
+            usage
+            ;;
+        :)
+            echo "Missing argument"
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND -1))
 
 dataset=$1
 
-[ "$#" -eq 1 ] || die "1 argument required for the dataset path, $# provided"
+[ "$#" -eq 1 ] || (echo "1 argument required for the dataset path, $# provided"; usage)
 
-docker run --gpus all -it -v "$PWD:/home/tensorflow/code" \
+docker run -it -v "$PWD:/home/tensorflow/code" \
     -v "${dataset}:/home/tensorflow/data" \
     -w /home/tensorflow/code \
-    od
+    ${image}
--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@
 
 usage() { echo "Usage: $0 [-c path/to/pipeline.config] [-m path/to/model/dir]" 1>&2; exit 1; }
 
-pipeline=models/my_faster_rcnn_resnet50_v1_640x640/pipeline.config
-model_dir=models/my_faster_rcnn_resnet50_v1_640x640
+pipeline=models/my_faster_rcnn_resnet50_v1_800x1333/pipeline.config
+model_dir=models/my_faster_rcnn_resnet50_v1_800x1333
 
 while getopts ":c:m:h" o; do
     case "${o}" in
@@ -30,4 +30,5 @@ done
 python ~/models/research/object_detection/model_main_tf2.py \
     --pipeline_config_path=${pipeline} \
     --model_dir=${model_dir} \
+    #--checkpoint_dir=${model_dir} \ # Uncomment this for evaluation
     --alsologtostderr
