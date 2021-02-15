5d42edec25319572160ec47409545ca265e96c20
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-02-14 19:27:52 -0800

    Load fiftyone dataset as persistent

--- utils/remove-dups.py
+++ utils/remove-dups.py
@@ -1,17 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import fiftyone as fo
 import sys
 
 def main():
-  if len(sys.argv) < 3:
+  if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} path/to/data_dir")
     return
 
   data_dir = sys.argv[1]
-  images_dir = sys.argv[2]
+  #images_dir = sys.argv[2]
 
   dataset = fo.Dataset.from_dir(data_dir, 
-      fo.types.TFObjectDetectionDataset,
-      images_dir=images_dir)
+      fo.types.CVATImageDataset)
+
+  dataset.persistent = True
+
+  print(dataset)
+  print(dataset.head())
 
 main()
