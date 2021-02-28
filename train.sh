5942520d99daacc13c03cae398b6495d7e4e717f
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-02-28 00:13:35 -0800

    Add option for checkpoint dir
    Adds an option to the evaluation to specify the checkpoint dir
    allowing a different model dir to place the evaluation tensorboard
    info in. This also updates the pipeline with the correct learning rate
    steps.

--- train.sh
+++ train.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-usage() { echo "Usage: $0 [-eh] [-c <cur_epoch>] [-n <num_steps>] [-p <epoch_steps>] [-k <checkpoint_steps>] [-c path/to/pipeline.config] [-m path/to/model/dir]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-eh] [-u <cur_epoch>] [-n <num_steps>] [-p <epoch_steps>] [-k <checkpoint_steps>] [-c path/to/pipeline.config] [-m path/to/model/dir] [-d path/to/checkpoint/dir]" 1>&2; exit 1; }
 
 pipeline=models/my_faster_rcnn_resnet50_v1_800x1333/pipeline.config
 model_dir=models/my_faster_rcnn_resnet50_v1_800x1333
+check_dir="${model_dir}"
 num_steps=200000
 
 # Training will stop after every epoch and 
@@ -12,7 +13,7 @@ epoch_steps=10000
 checkpoint_steps=1000
 cur_epoch=0
 
-while getopts ":ehc:m:n:p:k:u:" o; do
+while getopts ":ehc:m:n:p:k:u:d:" o; do
     case "${o}" in
         c)
             pipeline=${OPTARG}
@@ -38,6 +39,9 @@ while getopts ":ehc:m:n:p:k:u:" o; do
         u)
             cur_epoch=${OPTARG}
             ;;
+        d)
+            check_dir=${OPTARG}
+            ;;
         \?)
             echo "Invalid option"
             usage
@@ -55,7 +59,7 @@ if [ "$eval" = true ]; then
         --model_dir=${model_dir} \
         --alsologtostderr \
         --eval_timeout=0 \
-        --checkpoint_dir=${model_dir}
+        --checkpoint_dir=${check_dir}
 else
     for (( i=(cur_epoch + 1) * epoch_steps; i<=num_steps; i+=epoch_steps )); do
         python ~/models/research/object_detection/model_main_tf2.py \
