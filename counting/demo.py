f719bed7546c529a794c8dc5ad54e5ef52cc1c60
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-03-29 19:37:08 -0700

    Add code from the TF2 Obj Detection API tutorial

--- counting/demo.py
+++ counting/demo.py
@@ -1,14 +1,151 @@
 #!/usr/bin/env python
-import numpy as np
 import os
-import six.moves.urllib as urllib
-import sys
-import tarfile
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
+import pathlib
 import tensorflow as tf
-import zipfile
 
-from distutils.version import StrictVersion
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
+tf.get_logger().setLevel('ERROR')           # Suppress TensorFlow logging (2)
+
+IMAGE_PATHS = download_images()
+PATH_TO_MODEL_DIR = download_model(MODEL_NAME, MODEL_DATE)
+PATH_TO_LABELS = download_labels(LABEL_FILENAME)
+
+# Enable GPU dynamic memory allocation
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+# %%
+# Load the model
+# ~~~~~~~~~~~~~~
+# Next we load the downloaded model
+import time
+from object_detection.utils import label_map_util
+from object_detection.utils import config_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+
+PATH_TO_CFG = PATH_TO_MODEL_DIR + "/pipeline.config"
+PATH_TO_CKPT = PATH_TO_MODEL_DIR + "/checkpoint"
+
+print('Loading model... ', end='')
+start_time = time.time()
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(PATH_TO_CFG)
+model_config = configs['model']
+detection_model = model_builder.build(model_config=model_config, is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(PATH_TO_CKPT, 'ckpt-0')).expect_partial()
+
+@tf.function
+def detect_fn(image):
+    """Detect objects in image."""
+
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+
+    return detections
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Done! Took {} seconds'.format(elapsed_time))
+
+# %%
+# Load label map data (for plotting)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Label maps correspond index numbers to category names, so that when our convolution network
+# predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility
+# functions, but anything that returns a dictionary mapping integers to appropriate string labels
+# would be fine.
+
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
+                                                                    use_display_name=True)
+
+# %%
+# Putting everything together
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# The code shown below loads an image, runs it through the detection model and visualizes the
+# detection results, including the keypoints.
+#
+# Note that this will take a long time (several minutes) the first time you run this code due to
+# tf.function's trace-compilation --- on subsequent runs (e.g. on new images), things will be
+# faster.
+#
+# Here are some simple things to try out if you are curious:
+#
+# * Modify some of the input images and see if detection still works. Some simple things to try out here (just uncomment the relevant portions of code) include flipping the image horizontally, or converting to grayscale (note that we still expect the input image to have 3 channels).
+# * Print out `detections['detection_boxes']` and try to match the box locations to the boxes in the image.  Notice that coordinates are given in normalized form (i.e., in the interval [0, 1]).
+# * Set ``min_score_thresh`` to other values (between 0 and 1) to allow more detections in or to filter out more detections.
+import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')   # Suppress Matplotlib warnings
+
+def load_image_into_numpy_array(path):
+    """Load an image from file into a numpy array.
+
+    Puts image into numpy array to feed into tensorflow graph.
+    Note that by convention we put it into a numpy array with shape
+    (height, width, channels), where channels=3 for RGB.
+
+    Args:
+      path: the file path to the image
+
+    Returns:
+      uint8 numpy array with shape (img_height, img_width, 3)
+    """
+    return np.array(Image.open(path))
+
+
+for image_path in IMAGE_PATHS:
+
+    print('Running inference for {}... '.format(image_path), end='')
+
+    image_np = load_image_into_numpy_array(image_path)
+
+    # Things to try:
+    # Flip horizontally
+    # image_np = np.fliplr(image_np).copy()
+
+    # Convert image to grayscale
+    # image_np = np.tile(
+    #     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+
+    detections = detect_fn(input_tensor)
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes']+label_id_offset,
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=200,
+            min_score_thresh=.30,
+            agnostic_mode=False)
+
+    plt.figure()
+    plt.imshow(image_np_with_detections)
+    print('Done')
+plt.show()
