1576c497535541bf985a722b8f0181d5ade0aa0b
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-03-29 19:17:13 -0700

    Add prelim demo script

---
+++ counting/demo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from distutils.version import StrictVersion
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
