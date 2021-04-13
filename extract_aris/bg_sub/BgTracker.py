00b89af417c18cd0d21efb099107f89665ea351d
Yi Ching Chou <ycchou@sfu.ca>
2021-04-13 16:16:02 -0700

    Implement some tracker private functions and their unit tests

---
+++ extract_aris/bg_sub/BgTracker.py
@@ -0,0 +1,28 @@
+import math
+
+
+class Tracker:
+    def __init__(self, stats):
+        super().__init__()
+
+    def __is_same(self, xywh1, xywh2, radius):
+        centroid1 = self.__calculate_centroid(xywh1)
+        centroid2 = self.__calculate_centroid(xywh2)
+        diff_x = centroid1[0] - centroid2[0]
+        diff_y = centroid1[1] - centroid2[1]
+        diff = (diff_x, diff_y)
+        distance = math.sqrt(diff[0] ** 2 + diff[1] ** 2)
+        return distance <= radius
+
+    def __calculate_centroid(self, xywh):
+        (x, y, w, h) = self.__unpack_xywh(xywh)
+        centroid_x = w / 2 + x
+        centroid_y = h / 2 + y
+        return (centroid_x, centroid_y)
+
+    def __unpack_xywh(self, xywh):
+        x = xywh["x"]
+        y = xywh["y"]
+        w = xywh["w"]
+        h = xywh["h"]
+        return (x, y, w, h)
---
+++ extract_aris/bg_sub/BgTracker_test.py
@@ -0,0 +1,36 @@
+import unittest
+from BgTracker import *
+from BgObjLabel import *
+
+
+class TrackerTest(unittest.TestCase):
+
+    def __init__(self, methodName):
+        super().__init__(methodName)
+
+    def __build_stats(self):
+        return []
+
+    def test_calculate_centroid(self):
+        xywh = {"x": 1230, "y": 448, "w": 174, "h": 53}
+        tracker = Tracker(self.__build_stats())
+        centroid = tracker._Tracker__calculate_centroid(xywh)
+        self.assertEqual(centroid, (87+1230, 26.5+448))
+
+    def test_is_same_object_within_radius(self):
+        r = 5
+        xywh1 = {"x": 1230, "y": 448, "w": 174, "h": 53}
+        xywh2 = {"x": 1230 + r/2, "y": 448 + r/2, "w": 174, "h": 53}
+        tracker = Tracker(self.__build_stats())
+        self.assertTrue(tracker._Tracker__is_same(xywh1, xywh2, r))
+
+    def test_is_same_object_outside_radius(self):
+        r = 5
+        xywh1 = {"x": 1230, "y": 448, "w": 174, "h": 53}
+        xywh2 = {"x": 1230 + r/2 + 2, "y": 448 + r/2 + 2, "w": 174, "h": 53}
+        tracker = Tracker(self.__build_stats())
+        self.assertFalse(tracker._Tracker__is_same(xywh1, xywh2, r))
+
+
+if __name__ == '__main__':
+    unittest.main()
