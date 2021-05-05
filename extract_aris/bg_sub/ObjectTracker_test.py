b34a04a6565d4e0d32905b63a06c0e2520140acd
Yi Ching Chou <ycchou@sfu.ca>
2021-05-05 11:18:26 -0700

    Format the ObjectTracker test code

--- extract_aris/bg_sub/ObjectTracker_test.py
+++ extract_aris/bg_sub/ObjectTracker_test.py
@@ -22,19 +22,19 @@ class TrackerTest(unittest.TestCase):
         xywh = {"x": 1230, "y": 448, "w": 174, "h": 53}
         tracker = self.__build_tracker()
         centroid = tracker._ObjectTracker__calculate_centroid(xywh)
-        self.assertEqual(centroid, (87+1230, 26.5+448))
+        self.assertEqual(centroid, (87 + 1230, 26.5 + 448))
 
     def test_is_same_object_within_radius(self):
         r = 5
         xywh1 = {"x": 1230, "y": 448, "w": 174, "h": 53}
-        xywh2 = {"x": 1230 + r/2, "y": 448 + r/2, "w": 174, "h": 53}
+        xywh2 = {"x": 1230 + r / 2, "y": 448 + r / 2, "w": 174, "h": 53}
         tracker = self.__build_tracker()
         self.assertTrue(tracker._ObjectTracker__is_same(xywh1, xywh2, r))
 
     def test_is_same_object_outside_radius(self):
         r = 5
         xywh1 = {"x": 1230, "y": 448, "w": 174, "h": 53}
-        xywh2 = {"x": 1230 + r/2 + 2, "y": 448 + r/2 + 2, "w": 174, "h": 53}
+        xywh2 = {"x": 1230 + r / 2 + 2, "y": 448 + r / 2 + 2, "w": 174, "h": 53}
         tracker = self.__build_tracker()
         self.assertFalse(tracker._ObjectTracker__is_same(xywh1, xywh2, r))
 
@@ -43,7 +43,7 @@ class TrackerTest(unittest.TestCase):
         bgFrame = BgFrame()
         xywh = {"x": 1230, "y": 448, "w": 174, "h": 53}
         for i in range(10):
-            bgFrame.create_and_add_object(i+10, xywh)
+            bgFrame.create_and_add_object(i + 10, xywh)
         new_bgFrame = tracker._ObjectTracker__init_id(bgFrame)
 
         all_objects = new_bgFrame.get_all_objects()
@@ -70,7 +70,7 @@ class TrackerTest(unittest.TestCase):
 
         r = 5
         xywh1 = {"x": 1230, "y": 448, "w": 174, "h": 53}
-        xywh2 = {"x": 1230 + r/2, "y": 448 + r/2, "w": 174, "h": 53}
+        xywh2 = {"x": 1230 + r / 2, "y": 448 + r / 2, "w": 174, "h": 53}
 
         frame1.create_and_add_object(1, xywh1)
         frame2.create_and_add_object(2, xywh2)
