f14809bd3de2b2cc70b2fa74aca57bbba7485290
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-06-07 15:30:53 -0700

    Add option to use class ID instead

--- utils/gt_count.py
+++ utils/gt_count.py
@@ -23,25 +23,33 @@ def count_jde(args):
   counts = []
   with open(label_file, 'r') as f:
     for line in f:
-      counts.append((line, []))
+      counts.append((line.rstrip(), []))
 
+  print('Reading annotation files...')
   for anno_file in annos_paths:
     with open(anno_file, 'r') as f:
       for line in f:
-        parts = line.strip().split(',')
+        parts = line.strip().split(' ')
         class_id, track_id, *_ = deserialize_anno(parts)
         if not track_id in counts[class_id-1][1]:
           counts[class_id-1][1].append(track_id)
 
+  print('Writing ground truth counts...')
   with open('gt_counts.csv', 'w') as out:
-    out.write('class,count\n')
-    for categ in counts:
-      out.write(f"{categ[0]}, {len(categ[1])}\n")
+    if not args.id:
+      out.write('class,count\n')
+    for i, categ in enumerate(counts):
+      if args.id:
+        out.write(f"{i+1},{len(categ[1])}\n")
+      else:
+        out.write(f"{categ[0]},{len(categ[1])}\n")
+  print('Done.')
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Count ground truth classes')
 
   parser.add_argument('data_dir')
+  parser.add_argument('-i', '--id', action='store_true', help='Removes header and stores category names as IDs. Good for further scripts down the pipeline.')
   parser.set_defaults(func=count_jde)
 
   args = parser.parse_args()
