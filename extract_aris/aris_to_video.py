af514109b98e2bd5ee072598468b791cb1dbdbe7
David Chou <mrycchou@gmail.com>
2021-06-15 20:48:45 -0700

    Refactor code
    Long options have two dashes.
    Add MyArg class so we can access attributes with name instead of strings.
    Set frame_start and frame_end automatically if the user does not specify them.

--- extract_aris/aris_to_video.py
+++ extract_aris/aris_to_video.py
@@ -6,19 +6,26 @@ from bg_sub.BgUtility import BgUtility
 
 def extract_frames(source_file, output_file, frame_start, frame_end, skip, fps, invert):
     aris_data, frame = pyARIS.DataImport(source_file)
-    frame_extract = FrameExtract(aris_data)
-    frames = frame_extract.extract_frames(
-        frame_start, frame_end, skipFrame=skip)
-    BgUtility.export_video(
-        frames, output_file, invert_color=invert, fps=fps)
+    frame_start, frame_end = sanitize_frame_boundaries(frame_start, frame_end, aris_data)
+    frames = FrameExtract(aris_data).extract_frames(frame_start, frame_end, skipFrame=skip)
+    BgUtility.export_video(frames, output_file, invert_color=invert, fps=fps)
 
 
-def pack_arguments(args):
-    args_dict = vars(args)
-    pack = dict()
-    for arg_name in args_dict:
-        pack[arg_name] = pack_arg(args_dict[arg_name])
-    return pack
+def sanitize_frame_boundaries(frame_start, frame_end, aris_data):
+    if frame_start is None:
+        frame_start = 0
+    if frame_end is None:
+        frame_end = aris_data.FrameCount - 1
+    return frame_start, frame_end
+
+
+def build_my_args(arguments):
+    my_args = MyArgs()
+    arg_dict = vars(arguments)
+    for arg_name in arg_dict:
+        arg_value = pack_arg(arg_dict[arg_name])
+        setattr(my_args, arg_name, arg_value)
+    return my_args
 
 
 def pack_arg(arg):
@@ -37,6 +44,18 @@ def extract_single_arg(arg):
         return arg
 
 
+class MyArgs:
+    def __init__(self):
+        super().__init__()
+        self.source_file = None
+        self.output_file = None
+        self.s = None
+        self.e = None
+        self.skip = None
+        self.fps = None
+        self.invert = None
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert ARIS file to videos.')
 
@@ -44,13 +63,11 @@ if __name__ == '__main__':
     parser.add_argument('output_file', nargs=1, type=str, help='path of the output video')
     parser.add_argument('-s', nargs=1, type=int, help='frame start')
     parser.add_argument('-e', nargs=1, type=int, help='frame end')
-    parser.add_argument('-skip', nargs=1, type=int, help='skip frames for each exported frame', default=0)
-    parser.add_argument('-fps', nargs=1, type=int, help='frames per second', default=24)
-    parser.add_argument('-invert', nargs=1, type=bool, help='frames per second', default=False)
+    parser.add_argument('--skip', nargs=1, type=int, help='skip frames for each exported frame', default=0)
+    parser.add_argument('--fps', nargs=1, type=int, help='frames per second', default=24)
+    parser.add_argument('--invert', nargs=1, type=bool, help='invert frame color', default=False)
 
     args = parser.parse_args()
+    args = build_my_args(args)
 
-    pack_args = pack_arguments(args)
-
-    extract_frames(pack_args['source_file'], pack_args['output_file'], pack_args['s'], pack_args['e'],
-                   pack_args['skip'], pack_args['fps'], pack_args['invert'])
+    extract_frames(args.source_file, args.output_file, args.s, args.e, args.skip, args.fps, args.invert)
