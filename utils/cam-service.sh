e116680029a8f15611a13ed1388df53bb730a30e
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-07-06 02:57:21 -0700

    Add a comment on purpose of script

--- utils/cam-service.sh
+++ utils/cam-service.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+# This script is to be run by a systemd service to start the Jetson docker container and run the script to
+# start the camera detection/recording script.
 cd /mnt/ssd/jetson-inference
 /mnt/ssd/jetson-inference/docker/run.sh -v /mnt/ssd/homesecurity/:/homesecurity -r /homesecurity/cam.sh
