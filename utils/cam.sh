a2faa55ee1305c48539036bd1d70d7d4cb413b38
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-07-06 03:42:50 -0700

    Add extra params to cam recorder script

--- utils/cam.sh
+++ utils/cam.sh
@@ -1,2 +1,8 @@
 #!/bin/env bash
-ffmpeg -rtsp_transport tcp -i "rtsp://11.0.0.106/av0_0" -c:v h264_v4l2m2m -f segment -segment_time 3600 -strftime 1 "%m-%d-%Y %H-%M-%S Coquitlam Dam.mp4"
+url="rtsp://11.0.0.106/av0_0"
+#url="rtsp://10.0.0.98:554/user=admin&password=&channel=1&stream=0.sdp?"
+#encode="h264_v4l2m2m"
+encode=h264_omx
+scale="1280:-1"
+
+ffmpeg -rtsp_transport tcp -i "$url" -c:v "$encode" -vf scale="$scale" -f segment -segment_time 3600 -strftime 1 "%m-%d-%Y %H-%M-%S Coquitlam Dam.mp4"
