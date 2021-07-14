e4a8826c1250e120ab630f80dfc4223294252252
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-07-13 20:49:00 -0700

    Add useful info to setup raspberry pi

--- utils/useful_commands.sh
+++ utils/useful_commands.sh
@@ -7,3 +7,9 @@ sudo systemctl set-default multi-user.target
 # Use max power
 sudo nvpmodel -m 0
 sudo jetson_clocks
+
+# /etc/fstab
+# sshfs#pi@raspberrypi.local:/mnt/ssd/ /mnt/pi fuse defaults,_netdev,allow_other,reconnect 0 0
+
+# usbmount -- requires changing systemd-udevd service to "PrivateMounts=no"
+# NTFS: https://raspberrypi.stackexchange.com/questions/41959/automount-various-usb-stick-file-systems-on-jessie-lite
