c5eb964d0fa79ab2c1e8a8440e0c177645b6c01a
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-07-13 22:50:20 -0700

    Doc jetson-inference run.sh

--- utils/useful_commands.sh
+++ utils/useful_commands.sh
@@ -10,6 +10,7 @@ sudo jetson_clocks
 
 # /etc/fstab
 # sshfs#pi@raspberrypi.local:/media/usb0/ /mnt/pi-ssd fuse defaults,_netdev,allow_other,reconnect 0 0
+# Must update `jetson-inference/docker/run.sh` at the bottom, remove 't' from '-it' and add '--name jetson'
 
 # usbmount -- requires changing systemd-udevd service to "PrivateMounts=no"
 # NTFS: https://raspberrypi.stackexchange.com/questions/41959/automount-various-usb-stick-file-systems-on-jessie-lite
