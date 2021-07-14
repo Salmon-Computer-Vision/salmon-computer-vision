cc1dc203a7c674efb1e4811f19d0ddc56878dd4c
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-07-13 21:00:41 -0700

    Fix if statement

--- utils/cam.sh
+++ utils/cam.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-url="rtsp://11.0.0.106/av0_0"
+url="rtsp://10.0.0.89/av0_0"
 #url="rtsp://10.0.0.98:554/user=admin&password=&channel=1&stream=0.sdp?"
 #encode="h264_v4l2m2m"
 encode=h264_omx
@@ -8,6 +8,8 @@ dir=/media/usb0
 
 rec_dir="${dir}/record"
 
-if [ ! -d "${rec_dir}" ] && mkdir -p "$rec_dir"
+if [ ! -d "${rec_dir}" ]; then 
+    mkdir -p "$rec_dir"
+fi
 
 ffmpeg -rtsp_transport tcp -i "$url" -c:v "$encode" -vf scale="$scale" -f segment -segment_time 3600 -strftime 1 "${rec_dir}/%m-%d-%Y_%H-%M-%S_Coquitlam_Dam.mp4"
