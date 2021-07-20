f4de35872ef88a3634e8358f68d96cda9ab3bad6
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-07-19 20:24:10 -0700

    Fix volume mount to container

--- utils/cam-service.sh
+++ utils/cam-service.sh
@@ -2,4 +2,4 @@
 # This script is to be run by a systemd service to start the Jetson docker container and run the script to
 # start the camera detection/recording script.
 cd /home/salmonjetson/jetson-inference
-docker/run.sh -v /mnt/ssd/homesecurity/:/homesecurity -r /homesecurity/cam.sh
+docker/run.sh -v /home/salmonjetson/homesecurity/:/homesecurity -r /homesecurity/cam.sh
