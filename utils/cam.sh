1c3751a67f0b79d0b9788e00ccde8900f1e02ca1
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-07-23 23:57:38 -0700

    Fix encoding problems by skipping frames

--- utils/cam.sh
+++ utils/cam.sh
@@ -3,7 +3,7 @@ url="rtsp://10.0.0.89/av0_0"
 #url="rtsp://10.0.0.98:554/user=admin&password=&channel=1&stream=0.sdp?"
 #encode="h264_v4l2m2m"
 encode=h264_omx
-scale="1280:-1"
+scale="1280:720"
 dir=/media/usb0
 
 rec_dir="${dir}/record"
@@ -12,4 +12,4 @@ if [ ! -d "${rec_dir}" ]; then
     mkdir -p "$rec_dir"
 fi
 
-ffmpeg -rtsp_transport tcp -i "$url" -c:v "$encode" -vf scale="$scale" -f segment -segment_time 3600 -strftime 1 "${rec_dir}/%m-%d-%Y_%H-%M-%S_Coquitlam_Dam.mp4"
+ffmpeg -rtsp_transport tcp -r 25 -i "$url" -c:v "$encode" -vf scale="$scale" -r 15 -b:v 800k -an -f segment -segment_time 3600 -reset_timestamps 1 -strftime 1 "${rec_dir}/%m-%d-%Y_%H-%M-%S_Coquitlam_Dam.mp4"
