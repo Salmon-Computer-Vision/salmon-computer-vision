9048c0addad0284728e715ce064b2f2117020635
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-07-27 17:50:44 -0700

    Add substitute to allow for detached jetson docker

--- utils/cam-service.sh
+++ utils/cam-service.sh
@@ -2,4 +2,5 @@
 # This script is to be run by a systemd service to start the Jetson docker container and run the script to
 # start the camera detection/recording script.
 cd /home/salmonjetson/jetson-inference
+sed -i -r 's/ -it (--name jetson )?/ -i --name jetson /' docker/run.sh
 docker/run.sh -v /home/salmonjetson/homesecurity/:/homesecurity -r /homesecurity/cam.sh
