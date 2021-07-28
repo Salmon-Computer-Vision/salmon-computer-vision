23eefa3f260d8440e45b6ee71bccc77f04517211
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-07-28 00:28:47 -0700

    Allow for more than one jetson run

--- utils/cam-service.sh
+++ utils/cam-service.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # This script is to be run by a systemd service to start the Jetson docker container and run the script to
 # start the camera detection/recording script.
+name=$1
+dir_name=$2 # homesecurity
 cd /home/salmonjetson/jetson-inference
-sed -i -r 's/ -it (--name jetson )?/ -i --name jetson /' docker/run.sh
-docker/run.sh -v /home/salmonjetson/homesecurity/:/homesecurity -r /homesecurity/cam.sh
+sed -i -r "s/ -it (--name $name )?/ -i --name $name /" docker/run.sh
+sed -i -r "s/ -i (--name $name )?/ -i --name $name /" docker/run.sh
+docker/run.sh -v /home/salmonjetson/${dir_name}/:/${dir_name} -r /${dir_name}/cam.sh
