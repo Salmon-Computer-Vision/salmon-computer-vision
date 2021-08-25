240a1edeb8784e1db8acda30e591b6d46b4b016d
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-08-24 22:20:43 -0700

    Add date subfolder to recordings
    Requires one to use crontab to restart the service at midnight.
    This is in order to create the subfolder before recording.

--- utils/pi/cam.sh
+++ utils/pi/cam.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-url="rtsp://10.0.0.89/av0_0"
-#url="rtsp://10.0.0.98:554/user=admin&password=&channel=1&stream=0.sdp?"
+# Add the following to `sudo crontab -e`
+# 0 0 * * * systemctl restart cam-record.service
+
+#url="rtsp://10.0.0.89/av0_0"
+url="rtsp://10.0.0.98:554/user=admin&password=&channel=1&stream=0.sdp?"
 #encode="h264_v4l2m2m"
 encode=h264_omx
 scale="1280:720"
-dir=/media/usb0
+drive=/media/usb0
+dir=CoquitlamDamRecord
 suffix=$1
 
-rec_dir="${dir}/record"
+today=`date +'%m-%d-%Y'`
+rec_dir="${drive}/${dir}/${today}"
 
 if [ ! -d "${rec_dir}" ]; then 
     mkdir -p "$rec_dir"
