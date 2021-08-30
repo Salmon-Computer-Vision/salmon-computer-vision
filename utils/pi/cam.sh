239fc644501860c72a3e63caf74713d277ef8ae8
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-08-29 21:46:29 -0700

    Generalize cam.sh and use env vars in systemd

--- utils/pi/cam.sh
+++ utils/pi/cam.sh
@@ -3,16 +3,18 @@
 # 0 0 * * * systemctl restart cam-record.service
 
 #url="rtsp://10.0.0.89/av0_0"
-url="rtsp://10.0.0.98:554/user=admin&password=&channel=1&stream=0.sdp?"
+#url="rtsp://10.0.0.98:554/user=admin&password=&channel=1&stream=0.sdp?"
 #encode="h264_v4l2m2m"
+url=$1
+suffix=$2
+subdir=$3
 encode=h264_omx
 scale="1280:720"
 drive=/media/usb0
 dir="CoquitlamDamRecord"
-suffix=$1
 
 today=`date +'%m-%d-%Y'`
-rec_dir="${drive}/${dir}/${today}/${suffix}"
+rec_dir="${drive}/${dir}/${today}/${subdir}"
 
 if [ ! -d "${rec_dir}" ]; then 
     mkdir -p "$rec_dir"
