48d71bb5c42fea98cfba7f658352b841347b23ce
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-09-05 03:25:07 -0700

    Symlink plugins folder

--- utils/jetson/cam-service.sh
+++ utils/jetson/cam-service.sh
@@ -5,7 +5,11 @@ name=$1
 dir_name=$2 # homesecurity
 tmp_sh=$(mktemp)
 
+dir=/home/salmonjetson/${dir_name}/
+
 cd /home/salmonjetson/jetson-inference
 sed -r "s/ -it (--name $name )?/ -i --name $name /" docker/run.sh > $tmp_sh
 sed -i -r "s/ -i (--name .* )?(--rm)/ -i --name $name \2/" $tmp_sh
-bash $tmp_sh -c cam-detect -v /home/salmonjetson/${dir_name}/:/${dir_name} -r /${dir_name}/cam.sh
+
+sudo ln -s /tensorrt_demos/plugins plugins
+bash $tmp_sh -c cam-detect -v ${dir}:/${dir_name} -r /${dir_name}/cam.sh
