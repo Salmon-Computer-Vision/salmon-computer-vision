b872ea731b9748d2a52f9b41e6754318f87eb38a
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-09-12 23:12:41 -0700

    Add yolov4 tensorflow python command

--- utils/useful_commands.sh
+++ utils/useful_commands.sh
@@ -17,3 +17,6 @@ sudo jetson_clocks
 
 # Convert TF2 model to ONNX
 # python -m tf2onnx.convert --saved-model exported-models/ssd_mobilenet_320x320/saved_model --opset 13 --output ssd_mobilenet_320x320.onnx
+
+# Run tensorflow-yolov4-tflite tensorflow model
+# python detectvideo.py --weights checkpoints\yolov4-tiny-416 --tiny --size 416 --model yolov4 --video "09-25-2019 21-31-51 M Right Bank Underwater.mp4" --output det.mp4 --dis_cv2_window
