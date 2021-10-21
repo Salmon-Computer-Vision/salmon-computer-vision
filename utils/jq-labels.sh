9db4323b5f5c5b0c9acabefb104020822b834aca
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2021-10-20 18:32:01 -0700

    Update labels and jq query

--- utils/jq-labels.sh
+++ utils/jq-labels.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-jq '[.[] | .attributes[].mutable |= false]' $1 | \
-    jq '[.[] | .attributes[].values |= split("\n")]'
+jq '(.[].attributes[].mutable | if . == "False" then . else empty end) |= false' $1 | \
+    jq '(.[].attributes[].mutable | if . == "True" then . else empty end) |= true' | \
+    jq '(.[].attributes[].values | if type=="string" then . else empty end) |= split("\n")'
