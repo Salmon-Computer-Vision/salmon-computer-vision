535d776621365f78dabaa6c84661337a771fd410
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-02-02 18:04:14 -0800

    Update labels and update jq script for new XML format

--- utils/jq-labels.sh
+++ utils/jq-labels.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-jq '(.[].attributes[].mutable | if . == "False" then . else empty end) |= false' $1 | \
-    jq '(.[].attributes[].mutable | if . == "True" then . else empty end) |= true' | \
-    jq '(.[].attributes[].values | if type=="string" then . else empty end) |= split("\n")'
+jq '.[].attributes |= if type=="object" then .attribute else . end' $1 | \
+  jq '(.[].attributes[].mutable | if . == "False" then . else empty end) |= false' | \
+  jq '(.[].attributes[].mutable | if . == "True" then . else empty end) |= true' | \
+  jq '(.[].attributes[].values | if type=="string" then . else empty end) |= split("\n")'
