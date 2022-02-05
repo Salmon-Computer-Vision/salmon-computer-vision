1f3c2993fbb7a46c38c90ec379616da4d7841af2
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-02-05 03:12:12 -0800

    Parallelize filters because they can take a while

--- utils/split_by_categ.sh
+++ utils/split_by_categ.sh
@@ -17,10 +17,18 @@ fi
 
 echo "Species,Number" > "$cat_csv"
 cd "${dataset_dir}"
+# Filter in parallel
 while read label; do
     echo "Filter $label"
-    datum filter -e "/item[annotation/label=\"${label}\"]" -o "${label}" || true
-    num=$(datum info -p "$label" | grep -oP -m1 '(?<=length: ).*' || true)
+    datum filter -e "/item[annotation/label=\"${label}\"]" -o "${label}" || true &
+done < "$label_file"
+wait $(jobs -rp)
+
+while read label; do
+    echo "Creating project for $label"
+    datum create -o "${label}_datum" || true
+    datum import -p "${label}_datum" -n fish -f datumaro "$label" || true
+    num="$(datum info -p "${label}_datum" | grep -oP -m1 '(?<=length: ).*' || true)"
     if [ -z "$num" ]; then num=0; fi
     echo "${label},${num}" >> "$cat_csv"
 done < "$label_file"
