217059d23e6ef07f38eda48c51e8423c100032dd
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-02-20 00:32:11 -0800

    Add TODO to paralellize

--- utils/datum_extract_frames.sh
+++ utils/datum_extract_frames.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 min_num_param=2
 if [ "$#" -lt $min_num_param ]; then
@@ -9,19 +10,21 @@ fi
 source=$1
 dest=$2
 
-merged_dir="${dest}/merged"
-datum create -o "$merged_dir"
-for vid in "${source}"/*M*.m4v; do
+mkdir -p "$dest"
+#merged_dir="${dest}/merged"
+#datum create -o "$merged_dir"
+find "${source}" -name '*M*.m4v' ! -name '._*' | while read vid; do
     vid_base="${vid##*/}"
     vid_name="${vid_base%.*}"
     name="${vid_name// /_}" # Substitute spaces with underscores
     frames_dest="${dest}/${name}"
 
     echo "Splitting $vid_name"
+    # TODO: Paralellize with `sem`
     datum util split_video -i "$vid" -o "$frames_dest" -n "${name}_%06d"
-    if [ -d "${merged_dir}/annotations" ]; then
-        datum patch --overwrite "$merged_dir" "$frames_dest":image_dir
-    else
-        datum convert -i "$frames_dest" -if image_dir -f datumaro -o "$merged_dir"
-    fi
+    #if [ -d "${merged_dir}/annotations" ]; then
+    #    datum patch --overwrite "$merged_dir" "$frames_dest":image_dir
+    #else
+    #    datum convert -i "$frames_dest" -if image_dir -f datumaro -o "$merged_dir"
+    #fi
 done
