bf8a9c07f50c581a6e85ceb6e7f54e5b96bf145b
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-02-21 02:38:02 -0800

    Add module to convert iperf JSONs to CSV

---
+++ utils/plot_iperf/convert_iperf_to_csv.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# You must be in this folder to run this script
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <path/to/dataset_dir>"
+    echo "Converts all your JSON iperf logs to csv."
+    exit 0
+fi
+
+logs_dir="$1"
+dest_dir="$2"
+
+for dir in "${logs_dir}"/*/; do 
+    parallel python3 ./iperf-data-plot/main.py -o "${dest_dir}/$(basename $dir)" ::: "${dir}"/*.log
+done
