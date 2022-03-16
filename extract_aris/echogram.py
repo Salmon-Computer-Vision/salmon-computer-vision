4d93f942c03140b62ba857f556865acedada174e
Yi Ching Chou <ycchou@sfu.ca>
2022-03-16 14:07:40 -0700

    Fix echogram script

--- extract_aris/echogram.py
+++ extract_aris/echogram.py
@@ -8,20 +8,20 @@ source_file = '2020-05-27_071000.aris'
 
 
 def get_echogram(aris_data):
-    # num_frame = aris_data.FrameCount - 1
-    num_frame = 50
+    num_frame = aris_data.FrameCount - 1
     echogram = np.array([])
     for i in tqdm(range(0, num_frame)):  # aris_data.FrameCount - 1
         _frame = pyARIS.FrameRead(aris_data, i)
         echogram = np.append(echogram, get_vertical_line(_frame))
-    echogram = echogram.reshape(frame.frame_data.shape[0], num_frame)
+    echogram = echogram.reshape(num_frame, frame.frame_data.shape[0])
+    echogram = np.rot90(echogram)
     return echogram
 
 
 def get_vertical_line(frame):
     vline = np.array([])
     for row in range(frame.frame_data.shape[0]):
-        val = np.array(frame.frame_data[row]).mean()
+        val = np.array(frame.frame_data[row]).max()
         vline = np.append(vline, val)
     return vline
 
