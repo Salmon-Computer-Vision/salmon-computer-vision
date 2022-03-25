289bc50586b296cca2853be5c14557f456b596a9
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-03-24 21:46:03 -0700

    Fix station ID

--- utils/plot_iperf/get_weather_data.sh
+++ utils/plot_iperf/get_weather_data.sh
@@ -6,22 +6,23 @@ mkdir -p "$dest_dir"
 
 download_data() {
     dest_dir=$1
-    year=$2
-    month=$3
-    day=$4
+    station_id=$2
+    year=$3
+    month=$4
+    day=$5
 
     month_f=$(printf "%02d" $month)
     day_f=$(printf "%02d" $day)
 
-    wget -O "${dest_dir}/climate_hourly_BC_${year}-${month_f}-${day_f}.csv"  --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=${station_id}&Year=${year}&Month=${month}&Day=${day}&timeframe=1&submit=\ Download+Data"
+    wget -O "${dest_dir}/climate_hourly_BC_${year}-${month_f}-${day_f}.csv"  --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=${station_id}&Year=${year}&Month=${month}&Day=${day}&timeframe=1&submit= Download+Data"
 }
 
 export -f download_data
 
 for year in `seq 2022 2022`; do
-    for month in `seq 2 2`; do
-        for day in `seq 20 28`; do
-            sem -j 4  download_data "$dest_dir" "$year" "$month" "$day"
+    for month in `seq 3 3`; do
+        for day in `seq 1 23`; do
+            sem -j 4  download_data "$dest_dir" "$station_id" "$year" "$month" "$day"
         done
     done
 done
