c936bd1b7f9654ba21435e07e8443ddca6fcff3a
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-06-15 23:35:25 -0700

    Fix errors and missing data

--- utils/plot_iperf/convert_iperf_to_csv.sh
+++ utils/plot_iperf/convert_iperf_to_csv.sh
@@ -13,7 +13,7 @@ dest_dir="$2"
 
 IFS=$'\n'
 for dir in $(find "${logs_dir}" -type d); do 
-    if [ "$logs_dir" != "$dir" ]; then
+    if compgen -G "${dir}"/*.log > /dev/null && [ "$logs_dir" != "$dir" ]; then
         new_path="${dir#"$logs_dir"}"
         parallel python3 ./iperf-data-plot/main.py -o "${dest_dir}/${new_path}" ::: "${dir}"/*.log
     fi 
