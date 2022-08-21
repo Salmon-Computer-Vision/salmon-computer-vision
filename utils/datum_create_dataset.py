7237e8797a52f1176e754169f6a88dac26fefadb
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-08-20 17:48:35 -0700

    Start frame extraction function

--- utils/datum_create_dataset.py
+++ utils/datum_create_dataset.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
+import os
+import subprocess
+import datumaro as dm
+
 ### Required:
 # Datumaro
 # unzip
 
+DATUM = 'datum'
+def extract_frames(proj_path, name, vid_path):
+    # Extract frames to the project folder
+    dest_path = os.path.join(proj_path, name)
+    subprocess.run([DATUM, 'util', 'split_video', '-i', vid_path, '--image-ext=.png', "--name-pattern='frame_%06d'", '-o', dest_path])
+
+    name = os.path.splitext(os.path.basename(vid_path))[0]
 # For each video
 # Extract frames into a folder
 # Add the folder as the `image_dir` format
