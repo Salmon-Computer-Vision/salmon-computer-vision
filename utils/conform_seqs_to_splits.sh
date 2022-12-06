c9ffad2298c8abd7e09db5b5a1c72728bca2d708
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-12-06 13:07:44 -0800

    Check and remove dups in export

--- utils/conform_seqs_to_splits.sh
+++ utils/conform_seqs_to_splits.sh
@@ -9,6 +9,10 @@ for subset in ${subsets[@]}; do
     subset_path="${train_split_dir}/${subset}"
     for d in "$subset_path"/*; do
         dest_dir="${export_dir}/${subset}/${d##*/}"
+        if [ $(find "${export_dir}" -name "${d##*/}" | wc -l) = 2 ]; then
+            echo Duplicate at ${d##*/}
+            rm -r "$dest_dir"
+        fi
         if [ ! -d "$dest_dir" ]; then
             mv -v $(find "${export_dir}" -name "${d##*/}") "${export_dir}/${subset}"
         fi
