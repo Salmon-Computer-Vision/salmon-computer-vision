f42b53bc861e5c737acc75428649cbb5b9493613
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-12-06 23:35:14 -0800

    Add preprocess option and get stats before split

--- utils/datum_create_dataset.py
+++ utils/datum_create_dataset.py
@@ -48,6 +48,12 @@ KEY_ANNO = 'annotations'
 KEY_LABELS = 'labels'
 KEY_DISTRIB = 'distribution'
 
+MERGE_SEQ_STATS = 'merge_seq_stats.json'
+MERGE_SPECIES_COUNTER = 'merge_species_counter.json'
+
+SEQ_STATS = 'seq_stats.json'
+SPECIES_COUNTER = 'species_counter.json'
+
 VALID_SPLIT_RATIO = 0.15
 TEST_SPLIT_RATIO = 0.15
 RANDOM_SEED = 198
@@ -186,13 +192,13 @@ def export_vid(row_tuple):
     vid_data.gen_seqinfo(name)
     #vid_data.export_mot(name)
 
-class SeqDistrib:
-    name = ""
-    stats = {}
-
-    def __init__(self, name, stats):
-        self.name = name
-        self.stats = stats
+#class SeqDistrib:
+#    name = ""
+#    stats = {}
+#
+#    def __init__(self, name, stats):
+#        self.name = name
+#        self.stats = stats
 
 class MergeExport:
     dataset_merged = None
@@ -219,8 +225,7 @@ class MergeExport:
 
     @staticmethod
     def _merge_vid_job(row_tuple):
-        _, row, src_path, dest_folder, dataset_empty_path, \
-                seq_stats, species_counter, count_lock = row_tuple
+        _, row, src_path, dest_folder, dataset_empty_path = row_tuple
 
         name = filename_to_name(row.filename).lower()
         dest_path = osp.join(dest_folder, name.lower())
@@ -232,24 +237,6 @@ class MergeExport:
 
         # Fix duplicate labels
         dataset_merged.transform('remap_labels', mapping=DUP_LABELS_MAPPING)
-        stats = dmop.compute_ann_statistics(dataset_merged)
-        cat_distribs = stats[KEY_ANNO][KEY_LABELS][KEY_DISTRIB]
-        
-        seq_ds = SeqDistrib(name, cat_distribs)
-        for categ, count in cat_distribs.items():
-            if categ not in species_counter.keys():
-                species_counter[categ] = 0
-            if categ not in seq_stats.keys():
-                seq_stats[categ] = []
-            if count[0] == 0:
-                continue
-
-            count_lock.acquire()
-            temp_stats = seq_stats[categ]
-            temp_stats.append(seq_ds)
-            seq_stats[categ] = temp_stats
-            species_counter[categ] += count[0]
-            count_lock.release()
 
         dataset_merged.export(format='datumaro', save_dir=dest_path)
 
@@ -268,19 +255,15 @@ class MergeExport:
         species_counter = manager.dict()
         count_lock = manager.Lock()
 
-        row_merged_tuples = [tup + (self.src_path, self.merge_path, self.dataset_empty, 
-                seq_stats, species_counter, count_lock) for tup in self.df.iterrows()]
+        row_merged_tuples = [tup + (self.src_path, self.merge_path, self.dataset_empty) for tup in self.df.iterrows()]
         jobs_pool.map(self._merge_vid_job, row_merged_tuples)
 
         jobs_pool.close()
         jobs_pool.join()
 
-        self.merge_seq_stats = OrderedDict(sorted(seq_stats.items()))
-        self.merge_species_counter.update(species_counter)
-
     @staticmethod
     def _preprocess_job(row_tuple):
-        _, row, src_path, dest_folder, seq_stats, species_counter, count_lock, overwrite = row_tuple
+        _, row, src_path, dest_folder, overwrite = row_tuple
 
         name = filename_to_name(row.filename).lower()
         dest_path = osp.join(dest_folder, name.lower())
@@ -293,10 +276,34 @@ class MergeExport:
         data = dm.Dataset.import_from(seq_path, "datumaro")
         dataset_filtered_wide = dm.Dataset.filter(data, '/item[annotation/w > annotation/h] | /item/*[not(self::annotation)]')
 
-        stats = dmop.compute_ann_statistics(dataset_filtered_wide)
+        dataset_filtered_wide.export(format='datumaro', save_dir=dest_path)
+
+    def preprocess(self, overwrite=False):
+        """
+        Preprocess dataset (Removing tiny/incorrect boxes, etc.)
+        """
+        log.info('Preprocessing dataset...')
+        jobs_pool = Pool(self.jobs)
+
+        row_merged_tuples = [tup + (self.merge_path, self.preprocess_path, \
+                overwrite) for tup in self.df.iterrows()]
+        jobs_pool.map(self._preprocess_job, row_merged_tuples)
+
+        jobs_pool.close()
+        jobs_pool.join()
+
+    @staticmethod
+    def _get_stats_job(row_tuple):
+        _, row, src_path, seq_stats, species_counter, count_lock = row_tuple
+
+        name = filename_to_name(row.filename).lower()
+        seq_path = osp.join(src_path, name)
+        data = dm.Dataset.import_from(seq_path, "datumaro")
+
+        stats = dmop.compute_ann_statistics(data)
         cat_distribs = stats[KEY_ANNO][KEY_LABELS][KEY_DISTRIB]
         
-        seq_ds = SeqDistrib(name, cat_distribs)
+        seq_ds = (name, cat_distribs)
         for categ, count in cat_distribs.items():
             if categ not in species_counter.keys():
                 species_counter[categ] = 0
@@ -312,29 +319,6 @@ class MergeExport:
             species_counter[categ] += count[0]
             count_lock.release()
 
-        dataset_filtered_wide.export(format='datumaro', save_dir=dest_path)
-
-    def preprocess(self, overwrite=False):
-        """
-        Preprocess dataset (Removing tiny/incorrect boxes, etc.)
-        """
-        log.info('Preprocessing dataset...')
-        jobs_pool = Pool(self.jobs)
-        manager = Manager()
-        seq_stats = manager.dict()
-        species_counter = manager.dict()
-        count_lock = manager.Lock()
-
-        row_merged_tuples = [tup + (self.merge_path, self.preprocess_path, \
-                seq_stats, species_counter, count_lock, overwrite) for tup in self.df.iterrows()]
-        jobs_pool.map(self._preprocess_job, row_merged_tuples)
-
-        jobs_pool.close()
-        jobs_pool.join()
-
-        self.seq_stats = OrderedDict(sorted(seq_stats.items()))
-        self.species_counter.update(species_counter)
-
     def _get_seq_set(self, max_counts, counts):
         out_seqs = []
 
@@ -346,19 +330,19 @@ class MergeExport:
                 if counts[categ] >= max_counts[categ]:
                     break
                 seq = seqs.pop()
-                if seq.name in out_seqs:
+                if seq[0] in out_seqs:
                     continue
                 counts = self._count_categs(seq, counts)
-                out_seqs.append(seq.name)
+                out_seqs.append(seq[0])
 
         # Remove sequences taken out
         for categ in self.seq_stats.keys():
-            self.seq_stats[categ] = [seq for seq in self.seq_stats[categ] if seq.name not in out_seqs]
+            self.seq_stats[categ] = [seq for seq in self.seq_stats[categ] if seq[0] not in out_seqs]
 
         return (out_seqs, counts)
 
     def _count_categs(self, seq, counts):
-        for in_categ, count in seq.stats.items():
+        for in_categ, count in seq[1].items():
             if count[0] <= 0:
                 continue
             if not in_categ in counts:
@@ -366,14 +350,51 @@ class MergeExport:
             counts[in_categ] += count[0]
         return counts
 
-    def stratified_split(self, src_path, overwrite=False):
+    @staticmethod
+    def _to_json(_d, target_path):
+        d = benedict(_d)
+        d.to_json(filepath=target_path)
+
+
+    def stratified_split(self, src_path, skip_preprocess=False, overwrite=False):
         """
         Split the dataset into train, valid, and test sets using random stratified splitting
         """
         if not overwrite and osp.exists(self.split_path):
             log.info(f"Exists. Skipping stratified split {self.split_path}")
             return
+        log.info('Getting stats...')
+        jobs_pool = Pool(self.jobs)
+        manager = Manager()
+        seq_stats = manager.dict()
+        species_counter = manager.dict()
+        count_lock = manager.Lock()
+
+        row_merged_tuples = [tup + (src_path, \
+                seq_stats, species_counter, count_lock) for tup in self.df.iterrows()]
+        jobs_pool.map(self._get_stats_job, row_merged_tuples)
+
+        jobs_pool.close()
+        jobs_pool.join()
+
+        self.seq_stats = OrderedDict(sorted(seq_stats.items()))
+        self.species_counter.update(species_counter)
         log.info('Performing stratified split...')
+
+        # Save dicts
+        #self._to_json(self.seq_stats, osp.join(self.preprocess_path, SEQ_STATS))
+        #self._to_json(self.species_counter, osp.join(self.preprocess_path, SPECIES_COUNTER))
+        #if skip_preprocess:
+        #    if not self.merge_seq_stats:
+        #        self.merge_seq_stats = OrderedDict(sorted(benedict.from_json(osp.join(self.merge_path, MERGE_SEQ_STATS)).items()))
+        #        self.merge_species_counter = dict(benedict.from_json(osp.join(self.merge_path, MERGE_SPECIES_COUNTER)))
+        #    self.species_counter = self.merge_species_counter
+        #    self.seq_stats = self.merge_seq_stats
+        #else:
+        #    if not self.seq_stats:
+        #        self.seq_stats = sorted(OrderedDict(benedict.from_json(osp.join(self.preprocess_path, SEQ_STATS))))
+        #        self.species_counter = dict(benedict.from_json(osp.join(self.preprocess_path, SPECIES_COUNTER)))
+
         train_path = osp.join(self.split_path, 'train')
         valid_path = osp.join(self.split_path, 'valid')
         test_path = osp.join(self.split_path, 'test')
@@ -391,7 +412,7 @@ class MergeExport:
         # Shuffle seq category stats dict
         for categ in self.seq_stats.keys():
             # Sort first to elicit reproducibility
-            self.seq_stats[categ].sort(key=lambda x: x.name)
+            self.seq_stats[categ].sort(key=lambda x: x[0])
             self.rand.shuffle(self.seq_stats[categ])
 
         # Every set should have at least one seq of every category
@@ -404,14 +425,14 @@ class MergeExport:
                         seq = seqs.pop()
                     except:
                         continue
-                    if seq.name not in dataset_seqs:
-                        dataset_seqs.append(seq.name)
+                    if seq[0] not in dataset_seqs:
+                        dataset_seqs.append(seq[0])
                         counts = self._count_categs(seq, counts)
                         break
 
             # Remove
             for categ in self.seq_stats.keys():
-                self.seq_stats[categ] = [seq for seq in self.seq_stats[categ] if seq.name not in dataset_seqs]
+                self.seq_stats[categ] = [seq for seq in self.seq_stats[categ] if seq[0] not in dataset_seqs]
             return dataset_seqs, counts
 
         # Add one each to account for very small datasets
@@ -430,9 +451,9 @@ class MergeExport:
         # Export the rest as train set
         for categ, seqs in self.seq_stats.items():
             for seq in seqs:
-                if seq.name not in train_seqs:
+                if seq[0] not in train_seqs:
                     self._count_categs(seq, train_counts)
-                    train_seqs.append(seq.name)  
+                    train_seqs.append(seq[0])  
 
         log.info('Test')
         log.info(len(test_seqs))
@@ -456,14 +477,10 @@ class MergeExport:
         copy_seq(test_seqs, test_path)
         copy_seq(train_seqs, train_path)
 
-        def to_json(_d, target_path):
-            d = benedict(_d)
-            d.to_json(filepath=target_path)
-
-        to_json(self.species_counter, osp.join(self.split_path, 'distribution.json'))
-        to_json(test_counts, osp.join(self.split_path, 'test_distribution.json'))
-        to_json(valid_counts, osp.join(self.split_path, 'valid_distribution.json'))
-        to_json(train_counts, osp.join(self.split_path, 'train_distribution.json'))
+        self._to_json(self.species_counter, osp.join(self.split_path, 'distribution.json'))
+        self._to_json(test_counts, osp.join(self.split_path, 'test_distribution.json'))
+        self._to_json(valid_counts, osp.join(self.split_path, 'valid_distribution.json'))
+        self._to_json(train_counts, osp.join(self.split_path, 'train_distribution.json'))
 
     @staticmethod
     def _split_vid_job(row_tuple):
@@ -553,8 +570,12 @@ def main(args):
 
     # Merge and split inconsistent annotations and labels
     merge_exp.merge_dataset()
-    merge_exp.preprocess()
-    merge_exp.stratified_split(merge_exp.preprocess_path)
+    if not args.skip_preprocess:
+        processed_path = merge_exp.preprocess_path
+        merge_exp.preprocess()
+    else:
+        processed_path = merge_exp.merge_path
+    merge_exp.stratified_split(processed_path, args.skip_preprocess)
     #merge_exp.split_merged_dataset()
     #merge_dataset(df.iterrows(), merged_path, args.transform_path)
     #split_merged_dataset(df.iterrows(), merged_path, int(args.jobs))
@@ -576,6 +597,7 @@ if __name__ == '__main__':
     parser.add_argument('--export-path', default='export', help='Export path. Will create an export of the supplied format eg. `export_yolo` if `-f yolo`. Default: export')
     parser.add_argument('--exp-overwrite', action='store_true', help='Overwrite export.')
     parser.add_argument('--no-save-images', action='store_true', help='Toggle on to not save images on export.')
+    parser.add_argument('-s', '--skip-preprocess', action='store_true', help='Skip preprocessing.')
     parser.add_argument('-f', '--format', default='yolo', help='Export format. Check `datum export -h` for supported types. Default: yolo')
     parser.add_argument('-j', '--jobs', default='4', help='Number of jobs to run. Default: 4')
     parser.set_defaults(func=main)
