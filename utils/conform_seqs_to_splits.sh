0ef7052b064df07ef0422444750964e42c69acb9
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-12-27 04:25:40 -0800

    Update conform

--- utils/conform_seqs_to_splits.sh
+++ utils/conform_seqs_to_splits.sh
@@ -9,12 +9,15 @@ for subset in ${subsets[@]}; do
     subset_path="${train_split_dir}/${subset}"
     for d in "$subset_path"/*; do
         dest_dir="${export_dir}/${subset}/${d##*/}"
-        if [ $(find "${export_dir}" -name "${d##*/}" | wc -l) = 2 ]; then
+        if [ $(find "${export_dir}" -maxdepth 2 -name "${d##*/}" | wc -l) = 2 ]; then
             echo Duplicate at ${d##*/}
             rm -r "$dest_dir"
         fi
         if [ ! -d "$dest_dir" ]; then
-            mv -v $(find "${export_dir}" -name "${d##*/}") "${export_dir}/${subset}"
+            misplaced_seq=$(find "${export_dir}" -maxdepth 2 -name "${d##*/}")
+            if [ -d "$misplaced_seq" ]; then
+                mv -v "$misplaced_seq" "${export_dir}/${subset}"
+            fi
         fi
     done
 done
