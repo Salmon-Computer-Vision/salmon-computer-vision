31821baea4839194eafd5141f2be30ac5119450b
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-03-02 12:02:33 -0800

    Force yes and install for upgrading

--- utils/jetson/setup.sh
+++ utils/jetson/setup.sh
@@ -3,7 +3,7 @@
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true 
 
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y --force-yes Dpkg::Options::="--force-confnew"
 
 # Set text-only GUI for lower memory usage
 echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager
