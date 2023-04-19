dfbc6df2372d7ce367c3cdce1809ef8786dcd522
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-04-19 11:55:47 -0700

    Fix potential quoting issues

--- utils/pi/docker-kvs-run.sh
+++ utils/pi/docker-kvs-run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 name=$1
-rtsp_url=$2
+rtsp_url="$2"
 stream_name=$3
 
 docker run -i --rm --network host --name "$name" \
     -v /opt/vc:/opt/vc \
     546150905175.dkr.ecr.us-west-2.amazonaws.com/kinesis-video-producer-sdk-cpp-raspberry-pi \
-    "/opt/vc/rtsp_stream_kvs.sh" $rtsp_url $stream_name
+    "/opt/vc/rtsp_stream_kvs.sh" "$rtsp_url" $stream_name
