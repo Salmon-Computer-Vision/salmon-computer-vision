e3b80e4be33bd065d66e693832ad2f0de2111c4d
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-06-23 18:47:30 -0700

    Get whitelist IPs from file

--- utils/pi/veezoompro-kvs-run.sh
+++ utils/pi/veezoompro-kvs-run.sh
@@ -6,11 +6,12 @@ contains() {
     [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && exit(0) || exit(1)
 }
 
-whitelist=(IPS)
+whitelist_file=/home/salmonpi/salmon-computer-vision/utils/pi/whitelist.txt
 
 for ip in $(nmap -n -sn 192.168.1.0/24 -oG - | awk '/Up$/{print $2}'); do
-    if ! $(echo $whitelist | grep -w -q $ip); then
+    if ! $(cat $whitelist_file | grep -w -q $ip); then
         rtsp_url="rtsp://admin:admin@${ip}/1/2"
         /home/salmonpi/salmon-computer-vision/utils/pi/docker-kvs-run.sh $name "$rtsp_url" $stream_name
+        /usr/bin/docker exec -i $name /bin/sh -c "pkill -INT -f 'gst-launch-1.0'"
     fi
 done
