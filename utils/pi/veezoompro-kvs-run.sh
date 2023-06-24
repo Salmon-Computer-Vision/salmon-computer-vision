8854da9fb4fd1ff3b66094579e759433c0c8935e
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-06-23 18:56:23 -0700

    Stop docker container instead

--- utils/pi/veezoompro-kvs-run.sh
+++ utils/pi/veezoompro-kvs-run.sh
@@ -2,16 +2,12 @@
 name=$1
 stream_name=$2
 
-contains() {
-    [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && exit(0) || exit(1)
-}
-
 whitelist_file=/home/salmonpi/salmon-computer-vision/utils/pi/whitelist.txt
 
 for ip in $(nmap -n -sn 192.168.1.0/24 -oG - | awk '/Up$/{print $2}'); do
     if ! $(cat $whitelist_file | grep -w -q $ip); then
         rtsp_url="rtsp://admin:admin@${ip}/1/2"
         /home/salmonpi/salmon-computer-vision/utils/pi/docker-kvs-run.sh $name "$rtsp_url" $stream_name
-        /usr/bin/docker exec -i $name /bin/sh -c "pkill -INT -f 'gst-launch-1.0'"
+        /usr/bin/docker stop $name
     fi
 done
