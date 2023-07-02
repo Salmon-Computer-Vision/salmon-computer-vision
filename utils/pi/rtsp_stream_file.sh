12db1c5e05ecab2831f387710255d1fa0d809415
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-07-01 20:47:17 -0700

    Add stream to file just in case

---
+++ utils/pi/rtsp_stream_file.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rtsp_url="$1"
+stream_name=$2
+
+export LD_LIBRARY_PATH=/opt/awssdk/amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build/downloads/local/lib:$LD_LIBRARY_PATH
+export PATH=/opt/awssdk/amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build/downloads/local/bin:$PATH
+export GST_PLUGIN_PATH=/opt/awssdk/amazon-kinesis-video-streams-producer-sdk-cpp/kinesis-video-native-build/downloads/local/lib:$GST_PLUGIN_PATH
+
+gst-launch-1.0 -v rtspsrc location="${rtsp_url}" \
+    short-header=TRUE ! rtph264depay ! h264parse ! \
+    splitmuxsink location=/opt/vc/buffer/koeye%05d.mkv max-size-time=1200000000000 muxer=matroskamux
