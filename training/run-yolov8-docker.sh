d269f86ae774bc4b44e19f24b3a9e0a995299178
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-11-01 20:34:59 -0700

    Add data volumes and work on manipulating datasets

--- training/run-yolov8-docker.sh
+++ training/run-yolov8-docker.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --rm -it --ipc=host -p 8888:8888 --gpus all -v $PWD:/training -w /training yolov8-dev jupyter-lab --allow-root
+docker run --rm -it --ipc=host -p 8888:8888 --gpus all -v $PWD:/training -w /training \
+    -v /mnt/shiorissd4tb/masamim:/mnt/shiorissd4tb/masamim \
+    -v /home/masamim/salmon-computer-vision/utils/:/home/masamim/salmon-computer-vision/utils/ \
+    yolov8-dev jupyter-lab --allow-root
