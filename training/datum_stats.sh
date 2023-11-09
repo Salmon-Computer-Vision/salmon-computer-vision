1aba933677b580144082ded14acb74279d55851c
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-11-08 21:25:22 -0800

    Test YOLOv8 tracking and fix data stats

--- training/datum_stats.sh
+++ training/datum_stats.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 dir="$1"
 
-parallel datum stats --image-stats False {} ::: "$dir"/*
+#parallel datum stats --image-stats False {} ::: "$dir"/*
+
+i=0
+for d in "$dir"/*; do
+    (
+    mkdir $i
+    cd $i
+    datum stats --image-stats False $d &
+    )
+    ((i=i+1))
+done
