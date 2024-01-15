883da1a83bc84afa9d1449630f1eadb1a38f5b52
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2024-01-15 01:19:57 -0800

    Implement framework to figure when tracking disappears

--- training/run-yolov8-docker.sh
+++ training/run-yolov8-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker run --rm -it --ipc=host -p 8888:8888 --gpus all -v $PWD:/training -w /training \
-    -v /mnt/shiorissd4tb/masamim:/mnt/shiorissd4tb/masamim \
+    -v /mnt/shiorissd4tb:/mnt/shiorissd4tb \
     -v /home/masamim/salmon-computer-vision/utils/:/home/masamim/salmon-computer-vision/utils/ \
     yolov8-dev jupyter-lab --allow-root
