b5b627df6344f1fc0c4c6050cb8afb5f78842122
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2024-02-14 15:05:32 -0800

    Allow different trackers

--- training/pysalmcount/salmon_counter.py
+++ training/pysalmcount/salmon_counter.py
@@ -14,6 +14,8 @@ import pandas as pd
 VOTE_METHOD_IGN = 'ignore_thin'
 VOTE_METHOD_CONF = 'confidence'
 
+# tracker = botsort.yaml OR bytetrack.yaml
+
 class SalmonCounter:
     LEFT_PRE = 'l_'
     RIGHT_PRE = 'r_'
@@ -36,7 +38,7 @@ class SalmonCounter:
         self.tracking_thresh = tracking_thresh
         
 
-    def count(self, use_gt=False, save_vid=False, vote_method='ignore_thin', device=0):
+    def count(self, tracker="botsort.yaml", use_gt=False, save_vid=False, vote_method='ignore_thin', device=0):
         if vote_method not in [VOTE_METHOD_IGN, VOTE_METHOD_CONF]:
             raise ValueError(f'{vote_method} is not a valid method')
             
@@ -60,7 +62,7 @@ class SalmonCounter:
             confs = []
             if not use_gt:
                 # Run YOLOv8 tracking on the frame, persisting tracks between frames
-                results = self.model.track(item.frame, persist=True, verbose=False, device=device)
+                results = self.model.track(item.frame, tracker=tracker, persist=True, verbose=False, device=device)
 
                 orig_shape = results[0].orig_shape
                 # Get the boxes and track IDs
