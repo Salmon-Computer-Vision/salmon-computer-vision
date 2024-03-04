ab230ee587d8ae88f3b9572c2598d0f9b36b89b3
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2024-03-03 16:21:58 -0800

    Set mapping and finish conversion

--- training/tools/process_cvat_xml.py
+++ training/tools/process_cvat_xml.py
@@ -2,8 +2,25 @@
 import os
 import xml.etree.ElementTree as ET
 import concurrent.futures
-from datumaro.components.dataset import Dataset # Uncomment this line in the user's environment
+from threading import Lock
+from datumaro.components.dataset import Dataset
 import argparse
+
+DUP_LABELS_MAPPING = {
+        'White Fish': 'Whitefish',
+        'Bull Trout': 'Bull',
+        'Lan prey': 'Lamprey',
+        'Lampray': 'Lamprey',
+        'Coho Salmon': 'Coho',
+        'Rainbow Trout': 'Rainbow',
+        'Sockeye Salmon': 'Sockeye',
+        'Pink Salmon': 'Pink',
+        'Chinook Salmon': 'Chinook',
+        'Chum Salmon': 'Chum',
+        'Cutthroat Trout': 'Cutthroat',
+        'Juvenile Salmonoid': None,
+        'Juvenile coho salmon': None,
+        }
 
 def process_xml_file(file_path, output_base_dir):
     # Process a single XML file: rename '__instance_id' to 'track_id', convert to Datumaro format and export.
@@ -12,11 +29,11 @@ def process_xml_file(file_path, output_base_dir):
         tree = ET.parse(file_path)
         root = tree.getroot()
 
-        # Rename "__instance_id" to "track_id"
-        for elem in root.iter():
-            if "__instance_id" in elem.attrib:
-                elem.attrib["track_id"] = elem.attrib.pop("__instance_id")
-        
+        # Find the attribute with name "__instance_id" and rename it to "track_id"
+        for elem in root.iter('attribute'):
+            if elem.get('name') == '__instance_id':
+                elem.set('name', 'track_id')
+            
         # Save the modified XML to a new file in the new output directory
         relative_path = os.path.relpath(file_path, start=original_root_dir)
         new_file_path = os.path.join(output_base_dir, relative_path)
@@ -25,15 +42,24 @@ def process_xml_file(file_path, output_base_dir):
 
         # Convert to Datumaro format and export
         dataset = Dataset.import_from(new_file_path, format="cvat")
-        dataset = dataset.filter('/items/annotations', filter_annotations=True, remove_empty=True)
+        #breakpoint()
+        dataset = dataset.transform('remap_labels', mapping=DUP_LABELS_MAPPING)
+        dataset = dataset.filter('/item/annotation') # Must filter after remapping due to removed annotations
         output_dir = os.path.join(os.path.dirname(new_file_path), 'datumaro_format')
+        if os.path.exists(output_dir):
+            print(f"Exists skipping... {output_dir}")
+            return
         os.makedirs(output_dir, exist_ok=True)
-        dataset.export(output_dir, format='datumaro')
+        dataset.export(output_dir, format='datumaro', save_media=True)
         
     except ET.ParseError as e:
         print(f"Parse error processing file {file_path}: {e}")
     except Exception as e:
         print(f"Error processing file {file_path}: {e}")
+    finally:
+        with write_lock: # Safely write to error output files
+            with open(error_output_file_path, 'a') as f:
+                f.write(file_path + '\n')
 
 def find_xml_files(root_dir):
     # Recursively search for all 'output.xml' files within a given directory.
@@ -45,7 +71,12 @@ def find_xml_files(root_dir):
 def main(root_dir, output_base_dir, max_workers=None):
     # Main function to process all 'output.xml' files found in the given directory.
     global original_root_dir
+    global error_output_file_path
+    global write_lock
+    write_lock = Lock()
     original_root_dir = root_dir
+    error_output_file_path = os.path.join(output_base_dir, 'error_files.txt')
+    
     xml_files = list(find_xml_files(root_dir))
 
     # Process each file in parallel
