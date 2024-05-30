a519a1b30d523449f688369022ecb870c91d3f9c
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2024-05-30 16:08:47 -0700

    Update setup.sh

--- utils/jetson/setup.sh
+++ utils/jetson/setup.sh
@@ -3,10 +3,10 @@
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true 
 
-sudo apt-get update && sudo apt-get upgrade -y -o Dpkg::Options::="--force-confnew"
+#sudo apt-get update && sudo apt-get upgrade -y -o Dpkg::Options::="--force-confnew"
 
 # Do second round of upgrading
-sudo apt-get update && sudo apt-get upgrade -y -o Dpkg::Options::="--force-confnew"
+#sudo apt-get update && sudo apt-get upgrade -y -o Dpkg::Options::="--force-confnew"
 
 # Set text-only GUI for lower memory usage
 echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager
@@ -17,12 +17,12 @@ echo set shared/default-x-display-manager lightdm | debconf-communicate
 sudo nvpmodel -m 0
 sudo jetson_clocks
 
-sudo apt-get install -y git tmux
+#sudo apt-get install -y git tmux
 
-git clone --depth 1 https://github.com/Salmon-Computer-Vision/ByteTrack.git ~/ByteTrack
-mkdir -p ~/ByteTrack/YOLOX_outputs/yolox_nano_salmon
+#git clone --depth 1 https://github.com/Salmon-Computer-Vision/ByteTrack.git ~/ByteTrack
+#mkdir -p ~/ByteTrack/YOLOX_outputs/yolox_nano_salmon
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
+#sudo groupadd docker
+#sudo usermod -aG docker $USER
 
 # Reboot to finish setup
