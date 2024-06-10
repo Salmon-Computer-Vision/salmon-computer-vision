e54dae6a93f9ea9cd69fe01ce5e9e25291272437
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2024-06-09 21:54:07 -0700

    Add testing flag

--- training/tools/run_motion_detect_rtsp.py
+++ training/tools/run_motion_detect_rtsp.py
@@ -18,24 +18,28 @@ def get_orgid_and_site_name(name):
     return orgid, site_name, device_id
 
 
-def main(rtsp_url, save_folder, fps):
-    orgid, site_name, device_id = get_orgid_and_site_name(os.uname()[1])
-    site_save_path = os.path.join(save_folder, orgid, site_name, device_id)
+def main(args):
+    if args.test:
+        site_save_path = args.save_folder
+    else:
+        orgid, site_name, device_id = get_orgid_and_site_name(os.uname()[1])
+        site_save_path = os.path.join(args.save_folder, orgid, site_name, device_id)
 
     if not os.path.exists(site_save_path):
         os.makedirs(site_save_path)
 
-    vidloader = vl.VideoLoader([rtsp_url])
+    vidloader = vl.VideoLoader([args.rtsp_url])
 
     det = md.MotionDetector(vidloader, site_save_path)
-    det.run(fps=fps)
+    det.run(fps=args.fps)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Salmon Motion Detection and Video Clip Saving")
     parser.add_argument("rtsp_url", help="The RTSP URL of the camera")
     parser.add_argument("save_folder", help="Folder where video clips will be saved")
     parser.add_argument("--fps", default=None, help="Optionally set the FPS if it is not able to get it from the camera")
+    parser.add_argument("--test", action='store_true', help="Set this flag to not use site save path")
     args = parser.parse_args()
 
-    main(args.rtsp_url, args.save_folder, args.fps)
+    main(args)
 
