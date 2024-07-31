a9408fdd9dcc79d72596d3ad0f599dbe9226e37b
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2024-07-31 11:13:57 -0700

    Add function to re-encode to h264

--- utils/pi/tools/reencode_h264.py
+++ utils/pi/tools/reencode_h264.py
@@ -2,7 +2,11 @@
 
 import os
 import argparse
+import ffmpeg
+from pathlib import Path
 from pysalmcount import utils
+
+EXT = '.mp4'
 
 def gen_metadata(filename):
     metadata = utils.get_video_metadata(filename)
@@ -15,8 +19,19 @@ def gen_metadata(filename):
     else:
         logger.error(f"Could not generate metadata for file: {filename}")
 
+def reencode_h264(filepath):
+    input_path = Path(filepath)
+    tmp_filename = input_path.stem + '_tmp' + EXT
+    tmp_out_path = input_path.with_name(tmp_filename)
+    ffmpeg.input(filepath).output(tmp_out_path, vcodec='libx264', movflags='faststart').run()
+
+    # Overwrite old vid file
+
 def main(args):
     for filename in os.listdir(args.input):
+        # Check if metadata file exists
+        # Check if video file is not H264
+        # Re-encode video to H264
         gen_metadata(filename)
 
 if __name__ == "__main__":
