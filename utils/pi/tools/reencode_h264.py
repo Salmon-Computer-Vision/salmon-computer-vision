9ff13bb08b837bd8b2934888ae9e99f3d2cb43cc
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2024-08-14 11:11:42 -0700

    Archive then re-encode video

--- utils/pi/tools/reencode_h264.py
+++ utils/pi/tools/reencode_h264.py
@@ -8,8 +8,10 @@ from pathlib import Path
 
 from pysalmcount import utils
 from pysalmcount.motion_detect_stream import MOTION_VIDS_METADATA_DIR
+import shutil
 
 EXT = '.mp4'
+ARCHIVE_DIR = 'motion_vids_archive'
 
 def gen_metadata(filename):
     metadata = utils.get_video_metadata(filename)
@@ -22,33 +24,32 @@ def gen_metadata(filename):
     else:
         logger.error(f"Could not generate metadata for file: {filename}")
 
-def reencode_h264(filepath):
-    input_path = Path(filepath)
-    tmp_filename = input_path.stem + '_tmp' + EXT
-    tmp_out_path = input_path.with_name(tmp_filename)
-    ffmpeg.input(filepath).output(tmp_out_path, vcodec='libx264', movflags='faststart').run()
+def reencode_h264(filename: str, archive_dir: str):
+    filepath = Path(filename)
+    archive_path = Path(archive_dir) / filepath.name
+    shutil.move(filepath, archive_path)
 
-    # Overwrite old vid file
+    ffmpeg.input(archive_path).output(filepath, vcodec='libx264', movflags='faststart').run()
 
 def main(args):
+    input_dir_path = Path(args.input)
+    archive_path = input_dir_path.parent / ARCHIVE_DIR
+    archive_path.mkdir(exist_ok=True)
+
     for filename in os.listdir(args.input):
-        # Check if metadata file exists
         filepath = Path(filename)
+        metadata = utils.get_video_metadata(filename)
+
+        is_h264 = False
+        if metadata.codec_name != 'h264':
+            # Re-encode video to H264
+            reencode_h264(filename, archive_path)
+        else:
+            is_h264 = True
+
         metadata_path = filepath.parent / MOTION_VIDS_METADATA_DIR / (filepath.stem + '.json')
-        if metadata_path.exists():
-            # Check if video file is not H264
-            with open(str(metadata_path), 'r') as f:
-                metadata_dict = json.load(f)
-
-            metadata = utils.VideoMetadata(**metadata_dict)
-            # Generate metadata of the current vid
-            # Check that new generation is H264
-            if metadata.codec_name == 'h264':
-                continue
-        # Re-encode video to H264
-
-        # Create metadata if not exists or re-encoded
-        gen_metadata(filename)
+        if not is_h264 or not metadata_path.exists():
+            gen_metadata(filename)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Re-encodes video clips in a folder to H264 and re-generates their metadata.")
