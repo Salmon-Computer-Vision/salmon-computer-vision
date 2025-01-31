062fa3ba14ffd38e118925b56409d98767d66288
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2025-01-31 11:24:27 -0800

    Create script for downloading

---
+++ training/tools/download_extract_rclone.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+
+def main(args):
+    SOURCE = "aws"
+    MOTION_VID_DIR = "motion_vids"
+
+    download_cmd = ["rclone", "copy", "--log-level INFO"]
+
+    for filename in os.listdir(args.input):
+        print(filename)
+        parts = filename.split('-')
+        org = parts[0]
+        site = parts[1]
+
+        datetime_parts = parts[3].split('_')
+        device = f"{parts[2]}-{datetime_parts[0]}"
+
+        print(org, site, device)
+
+        download_cmd.append(f"{SOURCE}:{args.bucket}/{org}/{site}/{device}/{MOTION_VID_DIR}/{filename}")
+        download_cmd.append('.')
+
+        subprocess.run(download_cmd)
+        
+        break
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Downloads and extracts the frames and puts it next to YOLO labels.")
+    parser.add_argument("input", help="Input folder")
+    parser.add_argument("bucket", help="AWS Bucket")
+    args = parser.parse_args()
+
+    main(args)
