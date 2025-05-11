992de6f6be3b23c0978bf988a483d60a158b0f9d
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2025-05-11 15:01:01 -0700

    Allow mounting even if there's no samba

--- utils/pi/automount/automount.sh
+++ utils/pi/automount/automount.sh
@@ -6,7 +6,7 @@ DEVICE="/dev/${DEVBASE}"
 case "${ACTION}" in
     add)
         /usr/bin/mount -o user,auto,fmask=0000,dmask=0000 $DEVICE /media/hdd
-        /bin/systemctl restart smbd
+        /bin/systemctl restart smbd || true
         ;;
     remove)
         /bin/umount /media/hdd
