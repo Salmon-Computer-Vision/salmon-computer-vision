fca40f71196ea0ec4994bcff670e22b8c6c248f8
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2025-06-24 16:25:40 -0700

    Separate ext4 for its own mount method

--- utils/pi/automount/automount.sh
+++ utils/pi/automount/automount.sh
@@ -3,9 +3,15 @@ ACTION=$1
 DEVBASE=$2
 DEVICE="/dev/${DEVBASE}"
 
+FS_TYPE=$(blkid -s TYPE -o value "$DEVICE")
+
 case "${ACTION}" in
     add)
-        /usr/bin/mount -o user,auto,fmask=0000,dmask=0000 $DEVICE /media/hdd
+        if [ "$FS_TYPE" = "ext4" ]; then
+            /usr/bin/mount -t ext4 -o default $DEVICE /media/hdd
+        else
+            /usr/bin/mount -o user,auto,fmask=0000,dmask=0000 $DEVICE /media/hdd
+        fi
         /bin/systemctl restart smbd || true
         ;;
     remove)
