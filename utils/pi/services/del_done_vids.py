e99b270c44807d29bf1c6375fabead52294291a0
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2025-11-07 16:07:12 -0800

    Add script to delete done vids

---
+++ utils/pi/services/del_done_vids.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import run_all_videos
+
+def main():
+    vids = sorted(run_all_videos.VIDEO_DIR.rglob("Near.*.mp4"))
+
+    for vid in vids:
+        meta_path, lock_path = marker_paths(vid)
+
+        if is_done(vid):
+            try:
+                vid.unlink()
+                print(f"{vid} deleted successfully.")
+            except FileNotFoundError:
+                print(f"Error: {vid} not found.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
