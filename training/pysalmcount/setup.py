2c4ba4569ae39c8b023dced0f0ed08fb2220bb18
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2026-01-30 15:59:19 -0800

    Specify numpy version to prevent issues

--- training/pysalmcount/setup.py
+++ training/pysalmcount/setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='masamim@sfu.ca',
     # List of dependencies
     install_requires=[
-        'numpy',
+        'numpy==1.24.2',
         'pandas',
         'requests',
         'ffmpeg-python',
