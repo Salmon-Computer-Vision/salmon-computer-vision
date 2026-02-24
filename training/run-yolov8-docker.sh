67deec66b2aa310cb72922f186de746753950625
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2026-02-23 17:56:42 -0800

    Test conversions and counts compiles

--- training/run-yolov8-docker.sh
+++ training/run-yolov8-docker.sh
@@ -4,5 +4,6 @@ docker run --rm -it --ipc=host -p 8888:8888 --gpus all -v $PWD:/training -w /tra
     --device /dev/fuse --privileged \
     -v /mnt/shiorissd4tb:/mnt/shiorissd4tb \
     -v /mnt/ayumissd4tb:/mnt/ayumissd4tb \
+    -v /mnt/fuukassd:/mnt/fuukassd \
     -v /home/masamim/salmon-computer-vision/utils/:/home/masamim/salmon-computer-vision/utils/ \
     yolov8-dev jupyter-lab --allow-root
