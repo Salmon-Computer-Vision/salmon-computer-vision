479522d10cd705207da8678118bc181846b19341
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2026-04-10 18:31:45 -0700

    Set ray tmp

--- training/object-detection/src/object_detection/tuning/cli.py
+++ training/object-detection/src/object_detection/tuning/cli.py
@@ -57,10 +57,12 @@ def main() -> None:
 
             ray.init(
                 _temp_dir=str(ray_tmp_dir),
-                object_spilling_config=json.dumps({
-                    "type": "filesystem",
-                    "params": {"directory_path": str(spill_dir)}
-                }),
+                _system_config={
+                    "object_spilling_config": json.dumps({
+                        "type": "filesystem",
+                        "params": {"directory_path": str(spill_dir)}
+                    }),
+                },
                 ignore_reinit_error=True,
             )
 
