cedce0f3cb9b47a579711adf017c9c807b6d02ba
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2026-05-13 13:45:42 -0700

    Add site-specific plot template

--- training/object-detection/scripts/plot-all-eval.sh
+++ training/object-detection/scripts/plot-all-eval.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dvc plots diff $(dvc exp list --name-only | grep '^eval-site-')
+dvc plots diff -t config/site_resolve_independent_bars.vl.json $(dvc exp list --name-only | grep '^eval-site-')
