223529ab86017ded0c90e3825381648986bc5c2c
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2026-05-15 00:38:47 -0700

    Add vega template with values on bar plots

--- training/object-detection/scripts/plot-all-eval.sh
+++ training/object-detection/scripts/plot-all-eval.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dvc plots diff $(dvc exp list --name-only | grep '^eval-site-')
+dvc plots diff --title "$1" -t config/site_resolve_independent_bars_with_values.vl.json $(dvc exp list --name-only | grep '^eval-site-')
