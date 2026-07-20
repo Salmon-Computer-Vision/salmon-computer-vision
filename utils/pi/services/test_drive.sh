4140d247b009db7f6a16d3910e7e1d9620092a8f
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2026-07-20 11:32:36 -0700

    Send error ping regardless of unmounting

--- utils/pi/services/test_drive.sh
+++ utils/pi/services/test_drive.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 DRIVE=/media/hdd
 HC_URL=https://hc-ping.com/<destination_address>
@@ -7,5 +6,6 @@ HC_URL=https://hc-ping.com/<destination_address>
 if mountpoint "${DRIVE}" && touch "${DRIVE}/.testfile" && rm "${DRIVE}/.testfile"; then 
     curl -fsS -m 10 --retry 5 -o /dev/null "${HC_URL}"
 else 
-    umount "${DRIVE}" && curl -fsS -m 10 --retry 5 -o /dev/null "${HC_URL}/fail"
+    umount "${DRIVE}"
+    curl -fsS -m 10 --retry 5 -o /dev/null "${HC_URL}/fail"
 fi
